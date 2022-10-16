c886d2cf54de042a31f3a6d3fa7679f06cc73f43
Yu Mentsel <yu.mentsel@gmail.com>
2022-10-16 14:24:51 +0300

    feat: add solution - 21/21

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const mine = JSON.parse(JSON.stringify(matrix));
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      mine[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j]) {
+        let arr = [[i-1,j-1],[i-1,j],[i-1,j+1],[i,j-1],[i,j+1],[i+1,j-1],[i+1,j],[i+1,j+1]];
+        for (a of arr) {
+          if(a[0] >= 0 && a[0] <= matrix.length && a[1] >= 0 && a[1] <= matrix[0].length) {
+            mine[a[0]][a[1]]++;
+          }
+        }
+      }
+    }
+  }
+  return mine;
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let newArr = arr.filter(el => el !== -1).sort((a, b) => a - b);
+  let pos = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) pos.push(i);
+  }
+  for (let i = 0; i < pos.length; i++) {
+    newArr.splice(pos[i], 0, -1);
+  }
+  return newArr;
 }
 
 module.exports = {
